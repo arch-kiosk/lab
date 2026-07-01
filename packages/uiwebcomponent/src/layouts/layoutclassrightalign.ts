2e9153522fab1694283b697cfb1e7b8be2c1df4b
urapadmin <lutz_klein@brown.edu>
2026-07-01 18:28:28 -0400

    chore: clear directory to initialize subtree link

--- packages/uiwebcomponent/src/layouts/layoutclassrightalign.ts
+++
@@ -1,9 +0,0 @@
-import {UILayoutClass} from "./uilayoutclass";
-
-export class UIRightAlignLayoutClass extends UILayoutClass {
-    cssClass = "ui-right-align-layout"
-
-    renderLayoutStyles(): string {
-        return ""
-    }
-}
