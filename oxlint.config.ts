b8f497a04706f0f095eac7558041ae707518614c
urapadmin <lutz_klein@brown.edu>
2026-07-03 12:13:44 -0400

    linter config and stuff

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,7 +1,7 @@
 export default {
   rules: {
     "vite-plus/prefer-vite-plus-imports": "error",
-    // "no-console": "error",
+    // "no-console": "warn",
     "typescript/no-explicit-any": "warn",
   },
 };
